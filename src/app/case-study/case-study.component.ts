57d02b207d2d5e971376031cbea4c5bdc522e10a
isha-tagline <ishak.tagline@gmail.com>
2023-10-05 18:46:42 +0530

    Changes in section base binging

--- src/app/case-study/case-study.component.ts
+++ src/app/case-study/case-study.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./case-study.component.scss']
 })
 export class CaseStudyComponent {
+  public title : string = "Case Studies"
+  public caseArr : Case[] = [
+    {
+      src : 'assets/image/white-logo1.png',     
+      p : "Recognition, manage nominations, offer rewards, & automate service anniversaries. By developing such a recognized application."
+    },
+    {
+      src : 'assets/image/white-logo2.png',
+      p : "Replace confusing spreadsheets with marketing calendar software built for marketing teams."
+    },
+    {
+      src : 'assets/image/white-logo3.png',
+      p : "Make better stock investing decisions by joining the most exclusive community of stock investors. Learn, share & grow through authentic real-time discussion."
+    },
+    {
+      src : 'assets/image/white-logo4.png',
+      p : "Complete Business Solutions for your dental clinic."
+    },
+    {
+      src : 'assets/image/white-logo5.png',
+      p : "Build Healthy Habits. Stay Accountable. Share, Learn & Act on Insights."
+    },
+  ]
 
+
+  public imgPath1 : string = "assets/image/wrapper1.webp"
+  public imgPath2 : string = "assets/image/warpper2.webp"
+  public imgPath3 : string = "assets/image/wrapper3.webp"
+  public imgPath4 : string = "assets/image/wrapper5.webp"
+  public imgPath5 : string = "assets/image/wrapper6.webp"
+}
+
+
+interface Case{
+  src : string,
+  p:string
 }
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent {
+   public title : string = "We have the experience";
+   public title_detail : string = "We have got the experience to make your dream come true, and these stats speak for themselves: we have 350+ clients worldwide in only five years, and we have worked on over 650+ projects, all of which have been finished successfully and on schedule, with 100% client satisfaction. We also have 250K+ hours of freelancing experience."
 
+   public imgPath1 : string = 'assets/image/experience1.webp';
+   public imgPath2 : string = 'assets/image/experiance2.webp';
+   public imgPath3 : string = 'assets/image/experiance3.webp';
+   public imgPath4 : string = 'assets/image/experiance4.webp';
+
+   public expArr : ExpArr[] = [
+    {
+      detail : '150+',
+      p : 'Professional'
+    },
+    {
+      detail : '650+',
+      p : 'Completed Projects'
+    },
+    {
+      detail : '250+',
+      p : 'Freelance Hours'
+    },
+    {
+      detail : '50+',
+      p : 'Years of experience'
+    }
+   ]
+}
+
+interface ExpArr{
+  detail : string,
+  p : string
 }
